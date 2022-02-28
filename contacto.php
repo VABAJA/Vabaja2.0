a0f5bec0f4706df88a1c7cf2022bbf8b1057833b
jobarv <jbarrera@vabaja.com.mx>
2022-02-28 13:42:32 -0600

    se agrega formulario y pagina confirmacion

--- contacto.php
+++ contacto.php
@@ -1,11 +1,25 @@
 <?php
-if (isset($_POST['enviar'])) {
-    if (!empty($_POST['name'])) && !empty(&_POST['email']) && !empty(&_POST['telefono']) && !empty(&_POST['mensaje']) {
-        $name = $_POST['name'];
-        $telefono = $_POST['telefono'];
-        $mensaje = $_POST['mensaje'];
-        
-    }
-}
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$city = $_POST['ciudad'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Su ciudad es: " . $city . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'ventas@vabaja.com.mx';
+$asunto = 'Mensaje de Formulario Web';
+
+mail($para, $asunto, utf8_decode($message), $header);
+header("Location:confirmacion.html");
 ?>
