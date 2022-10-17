7e61d536331947c580d38bc46a335315da084fb2
jobarv <jbarrera@vabaja.com.mx>
2022-10-17 13:52:50 -0500

    cotizador

--- js/main.js
+++ js/main.js
@@ -9,3 +9,50 @@ botonEnviar.addEventListener('click', function() {
           });
           
 })
+
+function isMobile() {
+    if (sessionStorage.desktop)
+        return false;
+    else if (localStorage.mobile)
+        return true;
+    let mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
+    for (var i in mobile)
+        if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
+    return false;
+}
+
+const formulario = document.querySelector('#cotizador');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const telefono = '+528115028945';
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
+    Swal.fire({
+        title: '¡Gracias por contáctarnos!',
+        text: 'Ya tenemos la información de la refacción que buscas, en breve nos comunicaremos contigo.',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      })
+    buttonSubmit.disabled = true
+    setTimeout(() => {
+        let nombreCompleto = document.querySelector('#nombreCompleto').value
+        
+        let tipo = document.querySelector('#tipo').value
+        
+        let numeroParte = document.querySelector('#numeroParte').value
+        
+        let marca = document.querySelector('#marca').value
+
+        let mensaje = 'send?phone=' + telefono + '&text=*Busqueda de Refacción*%0A*Hola, mi nombre es*%0A' + nombreCompleto + '%0ANecesito un@' + tipo + '%0A*con número de parte*%0A' + numeroParte + '%0A*para un equipo de la marca*%0A' + marca + ''
+        if(isMobile()) {
+            window.open(urlMobile + mensaje, '_blank')
+        }else{
+            window.open(urlDesktop + mensaje, '_blank')
+        }
+        buttonSubmit.innerHTML = '<i class="fab fa-whatsapp"></i> Enviar WhatsApp'
+        buttonSubmit.disabled = false
+    }, 3000);
+});
